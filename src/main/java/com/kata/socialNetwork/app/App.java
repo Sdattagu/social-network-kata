155b101822a17b21bc29f0158e690ccabd5f9cc7
sdattagu <sdattagu@gmail.com>
2022-01-13 16:27:56 -0500

    Added buffered input reader, continuously read console

--- src/main/java/com/kata/socialNetwork/app/App.java
+++ src/main/java/com/kata/socialNetwork/app/App.java
@@ -1,14 +1,32 @@
 package com.kata.socialNetwork.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void runApp() {
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String input= "";
+            while(!input.equalsIgnoreCase("Exit")){
+                System.out.print("Input an action, or input 'Exit' to end the app> ");
+                input= reader.readLine();
+                System.out.println(input);
+            }
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
     }
 
+    public static void main( String[] args ) {
+        runApp();
+    }
 }
